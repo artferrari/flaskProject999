3cbeb36df8da0f3ca42b37be74fcb4218ef5056a
artferrari <39884968+artferrari@users.noreply.github.com>
2021-11-27 05:47:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,76 @@ from flask import Flask, render_template, request
 import coords as hist
 import current_loc
 import current_loc as curr
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from functools import wraps
+from flask import request, Response
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'}
+    )
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        print(auth)
+        #import pdb; pdb.set_trace()
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
+
+
 
 app = Flask(__name__)
 
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/test')
+@auth.login_required
+def test():
+    ret = Response("You are not logged in")
+    ret.headers['WWW-Authenticate'] = 'Basic'
+    ret.headers['Content-Type'] ='text/html'
+    return ret, 401
 
 @app.route("/")
+#@requires_auth
 def index():
     return render_template('Car Tracker.html')
 
 
 @app.route('/historical')
 def historial():
-    return hist.get_historical()
+    response = hist.get_historical()
+
+    return response
 
 
 @app.route('/current')
@@ -32,9 +90,9 @@ def getcoords():
         searchKey = reqData[10:]
         print(searchKey.decode("utf-8"))
         print(type(searchKey.decode("utf-8")))
-        coords = hist.searchCoords(searchKey.decode("utf-8"))
-        #print(coords)
-        return coords
+    coords = hist.searchCoords(searchKey.decode("utf-8"))
+    #print(coords)
+    return coords
 
 
 if __name__ == '__main__':
